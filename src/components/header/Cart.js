0310e65cd918f762ecb3955be15e220115160640
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-10-23 22:33:20 +0300

    style refactorings

--- src/components/header/Cart.js
+++ src/components/header/Cart.js
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import './cart.css';
 
 const Cart = () => {
+    const [showRemoveFromCartModal, setShowRemoveFromCartModal] =
+        useState(false);
+
+    const handleCloseRemoveFromCartModal = () => {
+        setShowRemoveFromCartModal(false);
+    };
+
+    const handleOpenRemoveFromCartModal = () => {
+        setShowRemoveFromCartModal(true);
+    };
+
+    console.log(showRemoveFromCartModal);
     return (
         <div className="dropdown">
             <button>Sepetim</button>
@@ -13,7 +26,9 @@ const Cart = () => {
                             Apple iPhone 11 Pro Maxi Phone 11 Pro Max iPhone 11
                             (Max 2 Line)...
                         </span>
-                        <button>Kaldır</button>
+                        <button onClick={handleOpenRemoveFromCartModal}>
+                            Kaldır
+                        </button>
                     </div>
                 </div>
 
@@ -24,7 +39,9 @@ const Cart = () => {
                             Apple iPhone 11 Pro Maxi Phone 11 Pro Max iPhone 11
                             (Max 2 Line)...
                         </span>
-                        <button>Kaldır</button>
+                        <button onClick={handleOpenRemoveFromCartModal}>
+                            Kaldır
+                        </button>
                     </div>
                 </div>
 
@@ -35,10 +52,44 @@ const Cart = () => {
                             Apple iPhone 11 Pro Maxi Phone 11 Pro Max iPhone 11
                             (Max 2 Line)...
                         </span>
-                        <button>Kaldır</button>
+                        <button onClick={handleOpenRemoveFromCartModal}>
+                            Kaldır
+                        </button>
                     </div>
                 </div>
             </div>
+
+            {showRemoveFromCartModal && (
+                <div
+                    className={`modal ${
+                        showRemoveFromCartModal ? 'modal-display' : ''
+                    }`}
+                >
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            Ürünü silmek istediğinize emin misiniz ?
+                        </div>
+                        <div className="modal-body">
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book. It has
+                            survived not only five centuries, but also the leap
+                            into electronic typesetting, remaining
+                            essentiall....
+                        </div>
+                        <div className="modal-footer">
+                            <button onClick={handleCloseRemoveFromCartModal}>
+                                EVET
+                            </button>
+                            <button onClick={handleCloseRemoveFromCartModal}>
+                                HAYIR
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };

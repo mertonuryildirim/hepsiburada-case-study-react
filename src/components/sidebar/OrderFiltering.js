512d4ebb2c38108a5c2b0a75973946301dae7888
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-10-24 14:45:00 +0300

    added order filtering functionality

--- src/components/sidebar/OrderFiltering.js
+++ src/components/sidebar/OrderFiltering.js
@@ -1,10 +1,48 @@
 const OrderFiltering = ({ handleFilteringDataChange }) => {
+    const handleOrderFilteringChange = (event) => {
+        handleFilteringDataChange(event);
+    };
     return (
         <div className="filter-options">
-            <span>En Düşük Fiyat</span>
-            <span>En Yüksek Fiyat</span>
-            <span>En Yeniler (A&gt;Z)</span>
-            <span>En Yeniler (Z&gt;A)</span>
+            <label>
+                <input
+                    type="checkbox"
+                    name="sortBy"
+                    value="price"
+                    onChange={(e) => handleOrderFilteringChange(e)}
+                />
+                <span>En Düşük Fiyat</span>
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="sortBy"
+                    value="price"
+                    onChange={(e) => handleOrderFilteringChange(e)}
+                />
+                <span>En Yüksek Fiyat</span>
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="sortBy"
+                    value="addedTime"
+                    onChange={(e) => handleOrderFilteringChange(e)}
+                />
+                <span>En Yeniler (A&gt;Z)</span>
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="sortBy"
+                    value="addedTime"
+                    onChange={(e) => handleOrderFilteringChange(e)}
+                />
+                <span>En Yeniler (Z&gt;A)</span>
+            </label>
         </div>
     );
 };

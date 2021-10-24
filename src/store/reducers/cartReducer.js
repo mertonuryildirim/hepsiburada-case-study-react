4672ecb4ddb11cf9a1423e466e2e8087ebc30f8b
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-10-24 22:58:49 +0300

    add & remove from cart localStorage functionality

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -2,28 +2,39 @@ import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/actionTypes';
 
 const initialState = {
     cartItems: [],
-    loading: false,
-    error: '',
 };
 
 const cartReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TO_CART:
-            //Control the added item.
             let addedItem = state.cartItems.find(
                 (cartItem) => cartItem.id === action.payload.id,
             );
-            //If item added before. Return the existing state
             if (addedItem) {
                 return { ...state, cartItems: state.cartItems };
-                //If item not added before. Its new item.
             } else {
+                localStorage.setItem(
+                    'cart',
+                    JSON.stringify({
+                        ...state,
+                        cartItems: [...state.cartItems, action.payload],
+                    }),
+                );
                 return {
                     ...state,
                     cartItems: [...state.cartItems, action.payload],
                 };
             }
         case REMOVE_FROM_CART:
+            localStorage.setItem(
+                'cart',
+                JSON.stringify({
+                    ...state,
+                    cartItems: state.cartItems.filter(
+                        (cartItem) => cartItem.id !== action.payload,
+                    ),
+                }),
+            );
             return {
                 ...state,
                 cartItems: state.cartItems.filter(

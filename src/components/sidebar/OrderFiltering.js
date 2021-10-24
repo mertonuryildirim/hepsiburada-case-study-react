c55e3ab8ae6978fa2843c217429099dbb3085f2b
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-10-24 15:14:01 +0300

    order filtering sidebar functionality changes

--- src/components/sidebar/OrderFiltering.js
+++ src/components/sidebar/OrderFiltering.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
+
 const OrderFiltering = ({ handleFilteringDataChange }) => {
-    const handleOrderFilteringChange = (event) => {
-        handleFilteringDataChange(event);
-    };
+    const [checkboxEvent, setCheckboxEvent] = useState({
+        sortPriceAsc: false,
+        sortPriceDesc: false,
+        sortAddedDesc: false,
+        sortAddedAsc: false,
+    });
+
     return (
         <div className="filter-options">
             <label>
                 <input
                     type="checkbox"
-                    name="sortBy"
-                    value="price"
-                    onChange={(e) => handleOrderFilteringChange(e)}
+                    name="sortPriceAsc"
+                    checked={checkboxEvent.sortPriceAsc}
+                    onChange={(event) => {
+                        handleFilteringDataChange(event);
+                        setCheckboxEvent({
+                            sortPriceAsc: true,
+                            sortPriceDesc: false,
+                            sortAddedDesc: false,
+                            sortAddedAsc: false,
+                        });
+                    }}
                 />
                 <span>En Düşük Fiyat</span>
             </label>
@@ -17,9 +31,17 @@ const OrderFiltering = ({ handleFilteringDataChange }) => {
             <label>
                 <input
                     type="checkbox"
-                    name="sortBy"
-                    value="price"
-                    onChange={(e) => handleOrderFilteringChange(e)}
+                    name="sortPriceDesc"
+                    checked={checkboxEvent.sortPriceDesc}
+                    onChange={(event) => {
+                        handleFilteringDataChange(event);
+                        setCheckboxEvent({
+                            sortPriceAsc: false,
+                            sortPriceDesc: true,
+                            sortAddedDesc: false,
+                            sortAddedAsc: false,
+                        });
+                    }}
                 />
                 <span>En Yüksek Fiyat</span>
             </label>
@@ -27,9 +49,17 @@ const OrderFiltering = ({ handleFilteringDataChange }) => {
             <label>
                 <input
                     type="checkbox"
-                    name="sortBy"
-                    value="addedTime"
-                    onChange={(e) => handleOrderFilteringChange(e)}
+                    name="sortAddedDesc"
+                    checked={checkboxEvent.sortAddedDesc}
+                    onChange={(event) => {
+                        handleFilteringDataChange(event);
+                        setCheckboxEvent({
+                            sortPriceAsc: false,
+                            sortPriceDesc: false,
+                            sortAddedDesc: true,
+                            sortAddedAsc: false,
+                        });
+                    }}
                 />
                 <span>En Yeniler (A&gt;Z)</span>
             </label>
@@ -37,9 +67,17 @@ const OrderFiltering = ({ handleFilteringDataChange }) => {
             <label>
                 <input
                     type="checkbox"
-                    name="sortBy"
-                    value="addedTime"
-                    onChange={(e) => handleOrderFilteringChange(e)}
+                    name="sortAddedAsc"
+                    checked={checkboxEvent.sortAddedAsc}
+                    onChange={(event) => {
+                        handleFilteringDataChange(event);
+                        setCheckboxEvent({
+                            sortPriceAsc: false,
+                            sortPriceDesc: false,
+                            sortAddedDesc: false,
+                            sortAddedAsc: true,
+                        });
+                    }}
                 />
                 <span>En Yeniler (Z&gt;A)</span>
             </label>

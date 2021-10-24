76e793eb85a59b9d68e32b3da8979b0d350fc50c
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-10-24 15:15:51 +0300

    subheader checkbox functionality changes

--- src/components/subHeader/Subheader.js
+++ src/components/subHeader/Subheader.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './subheader.css';
 
 const Subheader = ({ handleFilteringDataChange }) => {
-    const handleOrderFilteringChange = (event) => {
-        handleFilteringDataChange(event);
-    };
+    const [checkboxEvent, setCheckboxEvent] = useState({
+        sortPriceAsc: false,
+        sortPriceDesc: false,
+        sortAddedDesc: false,
+        sortAddedAsc: false,
+    });
     return (
         <div className="subheader">
             <div className="search-result">
@@ -23,9 +27,17 @@ const Subheader = ({ handleFilteringDataChange }) => {
                         <label>
                             <input
                                 type="checkbox"
-                                name="sortBy"
-                                value="price"
-                                onChange={(e) => handleOrderFilteringChange(e)}
+                                name="sortPriceAsc"
+                                checked={checkboxEvent.sortPriceAsc}
+                                onChange={(event) => {
+                                    handleFilteringDataChange(event);
+                                    setCheckboxEvent({
+                                        sortPriceAsc: true,
+                                        sortPriceDesc: false,
+                                        sortAddedDesc: false,
+                                        sortAddedAsc: false,
+                                    });
+                                }}
                             />
                             <span>En Düşük Fiyat</span>
                         </label>
@@ -33,9 +45,17 @@ const Subheader = ({ handleFilteringDataChange }) => {
                         <label>
                             <input
                                 type="checkbox"
-                                name="sortBy"
-                                value="price"
-                                onChange={(e) => handleOrderFilteringChange(e)}
+                                name="sortPriceDesc"
+                                checked={checkboxEvent.sortPriceDesc}
+                                onChange={(event) => {
+                                    handleFilteringDataChange(event);
+                                    setCheckboxEvent({
+                                        sortPriceAsc: false,
+                                        sortPriceDesc: true,
+                                        sortAddedDesc: false,
+                                        sortAddedAsc: false,
+                                    });
+                                }}
                             />
                             <span>En Yüksek Fiyat</span>
                         </label>
@@ -43,9 +63,17 @@ const Subheader = ({ handleFilteringDataChange }) => {
                         <label>
                             <input
                                 type="checkbox"
-                                name="sortBy"
-                                value="addedTime"
-                                onChange={(e) => handleOrderFilteringChange(e)}
+                                name="sortAddedDesc"
+                                checked={checkboxEvent.sortAddedDesc}
+                                onChange={(event) => {
+                                    handleFilteringDataChange(event);
+                                    setCheckboxEvent({
+                                        sortPriceAsc: false,
+                                        sortPriceDesc: false,
+                                        sortAddedDesc: true,
+                                        sortAddedAsc: false,
+                                    });
+                                }}
                             />
                             <span>En Yeniler (A&gt;Z)</span>
                         </label>
@@ -53,9 +81,17 @@ const Subheader = ({ handleFilteringDataChange }) => {
                         <label>
                             <input
                                 type="checkbox"
-                                name="sortBy"
-                                value="addedTime"
-                                onChange={(e) => handleOrderFilteringChange(e)}
+                                name="sortAddedAsc"
+                                checked={checkboxEvent.sortAddedAsc}
+                                onChange={(event) => {
+                                    handleFilteringDataChange(event);
+                                    setCheckboxEvent({
+                                        sortPriceAsc: false,
+                                        sortPriceDesc: false,
+                                        sortAddedDesc: false,
+                                        sortAddedAsc: true,
+                                    });
+                                }}
                             />
                             <span>En Yeniler (Z&gt;A)</span>
                         </label>

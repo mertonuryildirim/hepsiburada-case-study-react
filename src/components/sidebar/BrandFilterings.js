d40b50817c984690ead752d128ec361f6da6b633
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-10-24 14:54:54 +0300

    brand filtering functionality added

--- src/components/sidebar/BrandFilterings.js
+++ src/components/sidebar/BrandFilterings.js
@@ -1,13 +1,78 @@
 const BrandFilterings = ({ handleFilteringDataChange }) => {
+    const handleBrandFilteringChange = (event) => {
+        handleFilteringDataChange(event);
+    };
     return (
         <div className="filter-options">
-            <span>Samsung</span>
-            <span>Nokia</span>
-            <span>Apple</span>
-            <span>LG</span>
-            <span>Huawei</span>
-            <span>Xiaomi</span>
-            <span>General Mobile</span>
+            <label>
+                <input
+                    type="checkbox"
+                    name="brand"
+                    value="Samsung"
+                    onChange={(e) => handleBrandFilteringChange(e)}
+                />
+                <span>Samsung</span>
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="brand"
+                    value="Nokia"
+                    onChange={(e) => handleBrandFilteringChange(e)}
+                />
+                <span>Nokia</span>
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="brand"
+                    value="Apple"
+                    onChange={(e) => handleBrandFilteringChange(e)}
+                />
+                <span>Apple</span>
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="brand"
+                    value="LG"
+                    onChange={(e) => handleBrandFilteringChange(e)}
+                />
+                <span>LG</span>
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="brand"
+                    value="Huawei"
+                    onChange={(e) => handleBrandFilteringChange(e)}
+                />
+                <span>Huawei</span>
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="brand"
+                    value="Xiaomi"
+                    onChange={(e) => handleBrandFilteringChange(e)}
+                />
+                <span>Xiaomi</span>
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="brand"
+                    value="General Mobile"
+                    onChange={(e) => handleBrandFilteringChange(e)}
+                />
+                <span>General Mobile</span>
+            </label>
         </div>
     );
 };
